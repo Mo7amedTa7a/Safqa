394e2c692ba5c01827edf2d50f1103e522a50d0b
EsraaMamish-4 <esraa.mamish4@gmail.com>
2026-09-16 00:29:35 +0300

    app.routes.ts

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,3 +1,57 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ProductListComponent } from './features/products/product-list/ProductListComponent';
+import { ProductDetailsComponent } from './features/products/product-details/ProductDetailsComponent';
+import { CategoryListComponent } from './features/products/category-list/CategoryListComponent';
+
+import { BuyingRequestListComponent } from './features/buying-requests/my-requests/MyRequestsComponent';
+import { BuyingRequestCreateComponent } from './features/buying-requests/create-request/CreateRequestComponent';
+import { BuyingRequestEditComponent } from './features/buying-requests/edit-request/EditRequestComponent';
+import { BuyingRequestDetailsComponent } from './features/buying-requests/request-details/RequestDetailsComponent';
+
+import { AdminProductManagementComponent } from './features/products/product-management/ProductManagementComponent';
+
+
+export const routes: Routes = [
+
+  {
+    path: 'products',
+    component: ProductListComponent
+  },
+
+  {
+    path: 'products/:id',
+    component: ProductDetailsComponent
+  },
+
+  {
+    path: 'categories',
+    component: CategoryListComponent
+  },
+
+  {
+    path: 'buying-requests',
+    component: BuyingRequestListComponent
+  },
+
+  {
+    path: 'buying-requests/create',
+    component: BuyingRequestCreateComponent
+  },
+
+  {
+    path: 'buying-requests/:id/edit',
+    component: BuyingRequestEditComponent
+  },
+
+  {
+    path: 'buying-requests/:id',
+    component: BuyingRequestDetailsComponent
+  },
+
+  {
+    path: 'admin/products',
+    component: AdminProductManagementComponent
+  }
+
+];
